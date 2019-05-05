04d9cd8b911676128660ebf4d1f580bb83454920
Joe Papperello <joeyis@gmail.com>
2019-05-05 21:46:51 +0000

    doc

--- syllable/haiku.go
+++ syllable/haiku.go
@@ -6,13 +6,16 @@ import (
 	"strings"
 )
 
+// Haiku is a Paragraph(array of sentences) with some additional functionality for formatting Haikus for output
 type Haiku Paragraph
 
+// MarshalJSON satisfies the Marshaler interface, to JSONify a Haiku using ToStringArray()
 func (h Haiku) MarshalJSON() ([]byte, error) {
 	haikuStrArr := h.ToStringArray()
 	return json.Marshal(haikuStrArr)
 }
 
+// ToStringArray returns a string output of the haiku, where each item in the array[3] is a line of the haiku
 func (h Haiku) ToStringArray() [3]string {
 	haikuLines := [3]string{}
 	for lineIndex, line := range h {
@@ -33,14 +36,17 @@ func (h Haiku) ToStringArray() [3]string {
 	}
 	return haikuLines
 }
-func (h Haiku) ToStringSlice() []string {
+func (h Haiku) toStringSlice() []string {
 	haikuArr := h.ToStringArray()
 	return haikuArr[:]
 }
+
+// String stringifies the haiku for output
 func (h Haiku) String() string {
-	return strings.Join(h.ToStringSlice(), "\n")
+	return strings.Join(h.toStringSlice(), "\n")
 }
 
+// FinalWord returns the final word of the haiku. Used for filtering haikus that end in a 'dangling' word
 func (h Haiku) FinalWord() string {
 	finalLine := h[len(h)-1]
 	return finalLine[len(finalLine)-1].Word.Text
